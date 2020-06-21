876c4f99f62ceede4ab07a083fc37a3d8fb650c3
magicluminary <magicluminary@gmail.com>
2020-06-21 17:10:35 +0600

    axios 51, 52

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import styles from './Users.module.css'
+import * as axios from "axios";
+import userPhoto from '../../assets/images/ava.jpg'
 
 let Users = (props) => {
-    if (props.users.length === 0)
-        props.setUsers(
-            [
+    let getUsers = () => {
+
+
+        if (props.users.length === 0) {
+            const users = [
                 {
                     id: 1,
                     photo: 'https://www.nj.com/resizer/h8MrN0-Nw5dB5FOmMVGMmfVKFJo=/450x0/smart/cloudfront-us-east-1.images.arcpublishing.com/advancelocal/SJGKVE5UNVESVCW7BBOHKQCZVE.jpg',
                     followed: true,
-                    fullName: 'Dmitry',
+                    name: 'Dmitry',
                     status: 'Im a boss',
                     location: {city: 'Minsk', country: 'Belarus'}
                 },
@@ -17,28 +21,28 @@ let Users = (props) => {
                     id: 2,
                     photo: 'https://png.pngtree.com/png-clipart/20190920/original/pngtree-cute-chubby-little-dinosaur-png-image_4625217.jpg',
                     followed: false,
-                    fullName: 'Sasja',
+                    name: 'Sasja',
                     status: 'Im a boss too',
                     location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photo: 'https://www.news4jax.com/resizer/b89RYEm5oAgzxJxWIGoyLJ9lZu8=/960x960/smart/filters:format(jpeg):strip_exif(true):strip_icc(true):no_upscale(true):quality(65)/cloudfront-us-east-1.images.arcpublishing.com/gmg/X462YQ4HIJEGHHX2I3LXRV4G7A.jpg',
-                    followed: false,
-                    fullName: 'Andrew',
-                    status: 'Im a boss also',
-                    location: {city: 'Kiev', country: 'Ukraine'}
                 }
             ]
-        )
+
+            props.setUsers(users)
+
+            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+                props.setUsers(response.data.items)
+            })
+        }
+    }
 
     return (
         <div>
+            <button onClick={getUsers}>Get users</button>
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
                       <div>
-                          <img className="avatar" src={u.photo}/>
+                          {/*<img className="avatar" src={u.photos.small != null ? u.photos.small : userPhoto }/>*/}
                       </div>
                         <div>
                             {u.followed
@@ -52,12 +56,12 @@ let Users = (props) => {
                     </span>
                         <span>
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.city}</div>
-                            <div>{u.location.country}</div>
+                            <div>{"u.location.city"}</div>
+                            <div>{"u.location.country"}</div>
                         </span>
                     </span>
                     </div>
