853f01208ea736a7fd62a42341c3fc706d86b8be
magicluminary <magicluminary@gmail.com>
2020-07-11 20:28:18 +0600

    captcha 98

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -7,12 +7,14 @@ import {login} from "../../redux/auth-reducer";
 import {Redirect} from "react-router-dom";
 import styles from "../common/FormsControls/FormsControls.module.css"
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
     return (
         <form onSubmit={handleSubmit}>
-                {createField("Login","email",[required],Input)}
+            {createField("Login","email",[required],Input)}
             <div><Field placeholder={"Password"} name={"password"} type={"password"} component={Input} validate={[required]}/></div>
             <div><Field component={"input"} name={"rememberMe"} type={"checkbox"}/>remember me</div>
+            {captchaUrl && <img src={captchaUrl}/>}
+            {captchaUrl && createField("Symbols from image","captcha",[required],Input)}
             {error && <div className={styles.formSummaryError}>{error}</div>}
             <div><button>Login</button></div>
         </form>
@@ -22,7 +24,7 @@ const LoginReduxForm = reduxForm({form: 'login'})(LoginForm)
 
 const Login = (props) => {
     const onSubmit = (formData) => {
-        props.login(formData.email,formData.password,formData.rememberMe)
+        props.login(formData.email,formData.password,formData.rememberMe, formData.captcha)
     }
 
     if(props.isAuth){
@@ -31,12 +33,13 @@ const Login = (props) => {
 
     return <div>
         <h1>Login</h1>
-        <LoginReduxForm onSubmit={onSubmit} />
+        <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl}/>
     </div>
 }
 
 const mapStateToProps = (state) => ({
-    isAuth: state.auth.isAuth
+    isAuth: state.auth.isAuth,
+    captchaUrl: state.auth.captchaUrl
 })
 
 export default connect(mapStateToProps,{login})(Login);
